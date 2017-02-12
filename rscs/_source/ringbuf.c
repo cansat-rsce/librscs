93678432e98efe8a6d2c45838c3917a549b9a980
Kirs67 <kirilel200267@gmail.com>
2017-02-12 18:57:03 +0300

    Написал кольцевой буфер, скоро перепишу на него UART

---
+++ rscs/_source/ringbuf.c
@@ -0,0 +1,58 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "../error.h"
+
+#include "../ringbuf.h"
+
+struct rscs_ringbuf{
+	uint8_t * buffer; 	//Адрес буфера в памяти
+	size_t fullsize, //Полный размер буфера
+	size, //Размер записанных данных
+	head, //Смещение головы
+	tail; //Смещение хвоста
+};
+
+void rscs_ringbuf_init(rscs_ringbuf_t * buf, size_t bufsyze){
+	buf->buffer = (uint8_t *) malloc(bufsyze);
+	buf->fullsize = bufsyze;
+	buf->head = 0;
+	buf->tail = 0;
+	buf->size = 0;
+}
+
+void rscs_ringbuf_deinit(rscs_ringbuf_t * buf){
+	free(buf);
+}
+
+rscs_e rscs_ringbuf_push(rscs_ringbuf_t * buf, uint8_t value){
+	//Проверяем, есть ли место
+	if(buf->size == buf->fullsize) return RSCS_E_BUSY;
+	//Пишем значение в голову
+	buf->buffer[buf->head] = value;
+	//Двигаем голову
+	buf->head++;
+	if(buf->head == buf->fullsize) buf->head = 0;
+	//Увеличиваем размер записанного
+	buf->size++;
+
+	return RSCS_E_NONE;
+}
+
+rscs_e rscs_ringbuf_pop(rscs_ringbuf_t * buf, uint8_t * value){
+	//Проверяем, есть ли данные в буфере
+	if(buf->size == 0) return RSCS_E_BUSY;
+	//Читаем значение из кольцевого буфера
+	*value = buf->buffer[buf->tail];
+	//Двигаем хвост
+	buf->tail++;
+	if(buf->tail == buf->fullsize) buf->tail = 0;
+	//Уменьшаем размер записанного
+	buf->size++;
+
+	return RSCS_E_NONE;
+}
+
+size_t rscs_ringbuf_getsize(rscs_ringbuf_t * buf){
+	return buf->size;
+}
