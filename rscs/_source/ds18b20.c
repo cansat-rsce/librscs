6fcd709ab4dc87e2662e90079b499e28e083ecb1
Agnus <tenebris.agnus@yandex.ru>
2017-02-22 19:37:44 +0300

    Добавили функцию для проверки окончания замера, добавили rscs_ в название функций,

--- rscs/_source/ds18b20.c
+++ rscs/_source/ds18b20.c
@@ -22,7 +22,7 @@ struct rscs_ds18b20_t
 	uint64_t uid; // его уникальный идентификатор
 };
 
-rscs_ds18b20_t * ds18b20_init(uint64_t sensor_uid)
+rscs_ds18b20_t * rscs_ds18b20_init(uint64_t sensor_uid)
 {
 	// создаем дескриптор
 	rscs_ds18b20_t * retval = malloc(sizeof(rscs_ds18b20_t));
@@ -36,13 +36,13 @@ rscs_ds18b20_t * ds18b20_init(uint64_t sensor_uid)
 	return retval;
 }
 
-void ds18b20_deinit(rscs_ds18b20_t * sensor)
+void rscs_ds18b20_deinit(rscs_ds18b20_t * sensor)
 {
 	// просто освобождаем память дескриптора
 	free(sensor);
 }
 
-rscs_e ds18b20_start_conversion(rscs_ds18b20_t * sensor)
+rscs_e rscs_ds18b20_start_conversion(rscs_ds18b20_t * sensor)
 {
 	// Посылаем импульс сброса. Его необходимо посылать при каждом обращении к DS18B20.
 	if (!rscs_ow_reset())
@@ -54,7 +54,14 @@ rscs_e ds18b20_start_conversion(rscs_ds18b20_t * sensor)
 	return RSCS_E_NONE;
 }
 
-rscs_e ds18b20_read_temperature(rscs_ds18b20_t * sensor, int16_t * value_buffer)
+
+bool rscs_ds18b20_check_ready(void)
+{
+	return rscs_ow_read_bit();
+}
+
+
+rscs_e rscs_ds18b20_read_temperature(rscs_ds18b20_t * sensor, int16_t * value_buffer)
 {
 	// Посылаем импульс сброса. Его необходимо посылать при каждом обращении к DS18B20.
 	if (!rscs_ow_reset())
