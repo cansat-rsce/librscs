25c49ffcf26a6747ffff92df5de9edf36f711e7d
Kirs67 <kirilel200267@gmail.com>
2017-03-22 21:32:58 +0300

    Везде добавлено RSCS_, настройки CS вынесены в конфиг, добавлена отдельная функция пересчёта

--- rscs/_source/adxl345.c
+++ rscs/_source/adxl345.c
@@ -10,56 +10,56 @@
 
 
 /* Команды на чтение и запись */
-#define ADXL345_SPI_READ        (1 << 7)	//бит на чтение
-#define ADXL345_SPI_WRITE       (0 << 7)	//бит на запись
-#define ADXL345_SPI_MB          (1 << 6)	//бит чтение/запись нескольких байт (в противном случае только одного байта)
+#define RSCS_ADXL345_SPI_READ        (1 << 7)	//бит на чтение
+#define RSCS_ADXL345_SPI_WRITE       (0 << 7)	//бит на запись
+#define RSCS_ADXL345_SPI_MB          (1 << 6)	//бит чтение/запись нескольких байт (в противном случае только одного байта)
 
 /* ADXL345 Адреса регистров */
-#define ADXL345_DEVID			0x00 // R	Device ID
-#define ADXL345_OFSX            0x1E // R/W X-axis offset.
-#define ADXL345_OFSY            0x1F // R/W Y-axis offset.
-#define ADXL345_OFSZ            0x20 // R/W Z-axis offset.
-#define ADXL345_BW_RATE         0x2C // R/W Data rate and power mode control.
-#define ADXL345_POWER_CTL       0x2D // R/W Power saving features control.
-#define ADXL345_INT_ENABLE      0x2E // R/W Interrupt enable control.
-#define ADXL345_INT_MAP         0x2F // R/W Interrupt mapping control.
-#define ADXL345_INT_SOURCE      0x30 // R   Source of interrupts.
-#define ADXL345_DATA_FORMAT     0x31 // R/W Data format control.
-#define ADXL345_DATAX0          0x32 // R   X-Axis Data 0.
-#define ADXL345_DATAX1          0x33 // R   X-Axis Data 1.
-#define ADXL345_DATAY0          0x34 // R   Y-Axis Data 0.
-#define ADXL345_DATAY1          0x35 // R   Y-Axis Data 1.
-#define ADXL345_DATAZ0          0x36 // R   Z-Axis Data 0.
-#define ADXL345_DATAZ1          0x37 // R   Z-Axis Data 1.
-#define ADXL345_FIFO_CTL        0x38 // R/W FIFO control.
-#define ADXL345_FIFO_STATUS     0x39 // R   FIFO status.
+#define RSCS_ADXL345_DEVID			0x00 // R	Device ID
+#define RSCS_ADXL345_OFSX            0x1E // R/W X-axis offset.
+#define RSCS_ADXL345_OFSY            0x1F // R/W Y-axis offset.
+#define RSCS_ADXL345_OFSZ            0x20 // R/W Z-axis offset.
+#define RSCS_ADXL345_BW_RATE         0x2C // R/W Data rate and power mode control.
+#define RSCS_ADXL345_POWER_CTL       0x2D // R/W Power saving features control.
+#define RSCS_ADXL345_INT_ENABLE      0x2E // R/W Interrupt enable control.
+#define RSCS_ADXL345_INT_MAP         0x2F // R/W Interrupt mapping control.
+#define RSCS_ADXL345_INT_SOURCE      0x30 // R   Source of interrupts.
+#define RSCS_ADXL345_DATA_FORMAT     0x31 // R/W Data format control.
+#define RSCS_ADXL345_DATAX0          0x32 // R   X-Axis Data 0.
+#define RSCS_ADXL345_DATAX1          0x33 // R   X-Axis Data 1.
+#define RSCS_ADXL345_DATAY0          0x34 // R   Y-Axis Data 0.
+#define RSCS_ADXL345_DATAY1          0x35 // R   Y-Axis Data 1.
+#define RSCS_ADXL345_DATAZ0          0x36 // R   Z-Axis Data 0.
+#define RSCS_ADXL345_DATAZ1          0x37 // R   Z-Axis Data 1.
+#define RSCS_ADXL345_FIFO_CTL        0x38 // R/W FIFO control.
+#define RSCS_ADXL345_FIFO_STATUS     0x39 // R   FIFO status.
 
 /* ADXL345_POWER_CTL Определение регистра */
-#define ADXL345_PCTL_LINK       (1 << 5)
-#define ADXL345_PCTL_AUTO_SLEEP (1 << 4)
-#define ADXL345_PCTL_MEASURE    (1 << 3)
-#define ADXL345_PCTL_SLEEP      (1 << 2)
-#define ADXL345_PCTL_WAKEUP(x)  ((x) & 0x3)
+#define RSCS_ADXL345_PCTL_LINK       (1 << 5)
+#define RSCS_ADXL345_PCTL_AUTO_SLEEP (1 << 4)
+#define RSCS_ADXL345_PCTL_MEASURE    (1 << 3)
+#define RSCS_ADXL345_PCTL_SLEEP      (1 << 2)
+#define RSCS_ADXL345_PCTL_WAKEUP(x)  ((x) & 0x3)
 /* ADXL345_INT_ENABLE / ADXL345_INT_MAP / ADXL345_INT_SOURCE Определение регистра */
-#define ADXL345_DATA_READY      (1 << 7)
-#define ADXL345_SINGLE_TAP      (1 << 6)
-#define ADXL345_DOUBLE_TAP      (1 << 5)
-#define ADXL345_ACTIVITY        (1 << 4)
-#define ADXL345_INACTIVITY      (1 << 3)
-#define ADXL345_FREE_FALL       (1 << 2)
-#define ADXL345_WATERMARK       (1 << 1)
-#define ADXL345_OVERRUN         (1 << 0)
+#define RSCS_ADXL345_DATA_READY      (1 << 7)
+#define RSCS_ADXL345_SINGLE_TAP      (1 << 6)
+#define RSCS_ADXL345_DOUBLE_TAP      (1 << 5)
+#define RSCS_ADXL345_ACTIVITY        (1 << 4)
+#define RSCS_ADXL345_INACTIVITY      (1 << 3)
+#define RSCS_ADXL345_FREE_FALL       (1 << 2)
+#define RSCS_ADXL345_WATERMARK       (1 << 1)
+#define RSCS_ADXL345_OVERRUN         (1 << 0)
 /* ADXL345_DATA_FORMAT Определение регистра */
-#define ADXL345_SELF_TEST       (1 << 7)
-#define ADXL345_SPI             (1 << 6)
-#define ADXL345_INT_INVERT      (1 << 5)
-#define ADXL345_FULL_RES        (1 << 3)
-#define ADXL345_JUSTIFY         (1 << 2)
-#define ADXL345_RANGE(x)        ((x) & 0x3)
+#define RSCS_ADXL345_SELF_TEST       (1 << 7)
+#define RSCS_ADXL345_SPI_BIT             (1 << 6)
+#define RSCS_ADXL345_INT_INVERT      (1 << 5)
+#define RSCS_ADXL345_FULL_RES        (1 << 3)
+#define RSCS_ADXL345_JUSTIFY         (1 << 2)
+#define RSCS_ADXL345_RANGE(x)        ((x) & 0x3)
 
 /* ADXL345 Full Resolution Scale Factor */
-#define ADXL345_SCALE_FACTOR    		0.0039
-#define ADXL345_OFFSET_SCALE_FACTOR		15.6
+#define RSCS_ADXL345_SCALE_FACTOR    		0.0039
+#define RSCS_ADXL345_OFFSET_SCALE_FACTOR		15.6
 
 #define GOTO_END_IF_ERROR(X) if ((error = (X)) != RSCS_E_NONE) goto end;
 
@@ -88,13 +88,13 @@ void rscs_adxl345_CS_State(rscs_adxl345_t * device, bool state)
 
 
 /*ЧТЕНИЕ ЗНАЧЕНИЯ ИЗ РЕГИСТРА*/
-rscs_e rscs_adxl345_getRegisterValue(rscs_adxl345_t * device, uint8_t registerAddress, uint16_t/*!!!*/ * read_data)
+rscs_e rscs_adxl345_getRegisterValue(rscs_adxl345_t * device, uint8_t registerAddress, uint8_t * read_data)
 {
 	rscs_e error = RSCS_E_NONE;
 
 	switch (device->interface) {
 		case RSCS_ADXL345_SPI:
-			registerAddress = ADXL345_SPI_READ | registerAddress;	//дописываем в адрес бит чтения
+			registerAddress = RSCS_ADXL345_SPI_READ | registerAddress;	//дописываем в адрес бит чтения
 
 			rscs_adxl345_CS_State(device, 0);
 			rscs_spi_do(0x00);
@@ -129,7 +129,7 @@ rscs_e rscs_adxl345_setRegisterValue(rscs_adxl345_t * device, uint8_t registerAd
 
 	switch (device->interface) {
 		case RSCS_ADXL345_SPI:
-			registerAddress = ADXL345_SPI_WRITE | registerAddress;	//дописываем в адрес бит записи
+			registerAddress = RSCS_ADXL345_SPI_WRITE | registerAddress;	//дописываем в адрес бит записи
 
 			rscs_adxl345_CS_State(device, 0);
 			rscs_spi_write(&registerAddress, 1);
@@ -209,9 +209,9 @@ rscs_e rscs_adxl345_set_range(rscs_adxl345_t * device, rscs_adxl345_range_t rang
 	rscs_e error = RSCS_E_NONE;
 	uint8_t data = 0;
 
-	GOTO_END_IF_ERROR(rscs_adxl345_getRegisterValue(device, ADXL345_DATA_FORMAT, &data));
-	data = (data & 0x3) | ADXL345_RANGE(range);	//очищаем 2 младших бита регистра BW_RATE и записываем новое значение
-	GOTO_END_IF_ERROR(rscs_adxl345_setRegisterValue(device, ADXL345_DATA_FORMAT, data));
+	GOTO_END_IF_ERROR(rscs_adxl345_getRegisterValue(device, RSCS_ADXL345_DATA_FORMAT, &data));
+	data = (data & 0x3) | RSCS_ADXL345_RANGE(range);	//очищаем 2 младших бита регистра BW_RATE и записываем новое значение
+	GOTO_END_IF_ERROR(rscs_adxl345_setRegisterValue(device, RSCS_ADXL345_DATA_FORMAT, data));
 
 	device->range = range;
 
@@ -226,9 +226,9 @@ rscs_e rscs_adxl345_set_rate(rscs_adxl345_t * device, rscs_adxl345_rate_t rate)
 	rscs_e error = RSCS_E_NONE;
 	uint8_t data = 0;
 
-	GOTO_END_IF_ERROR(rscs_adxl345_getRegisterValue(device, ADXL345_BW_RATE, &data));
-	data = (data & 0xF) | ADXL345_RATE(rate);	//очищаем 4 младших бита регистра BW_RATE и записываем новое значение
-	GOTO_END_IF_ERROR(rscs_adxl345_setRegisterValue(device, ADXL345_BW_RATE, data));
+	GOTO_END_IF_ERROR(rscs_adxl345_getRegisterValue(device, RSCS_ADXL345_BW_RATE, &data));
+	data = (data & 0xF) | RSCS_ADXL345_RATE(rate);	//очищаем 4 младших бита регистра BW_RATE и записываем новое значение
+	GOTO_END_IF_ERROR(rscs_adxl345_setRegisterValue(device, RSCS_ADXL345_BW_RATE, data));
 
 	end:
 	return error;
@@ -244,17 +244,17 @@ rscs_e rscs_adxl345_set_offset(rscs_adxl345_t * device, float mg_x, float mg_y, 
 	int8_t ofs_y;
 	int8_t ofs_z;
 
-	if (mg_x < 0) ofs_x = !(round(- mg_x / ADXL345_OFFSET_SCALE_FACTOR) - 1);
-	if (mg_y < 0) ofs_y = !(round(- mg_y / ADXL345_OFFSET_SCALE_FACTOR) - 1);
-	if (mg_z < 0) ofs_z = !(round(- mg_z / ADXL345_OFFSET_SCALE_FACTOR) - 1);
+	if (mg_x < 0) ofs_x = !(round(- mg_x / RSCS_ADXL345_OFFSET_SCALE_FACTOR) - 1);
+	if (mg_y < 0) ofs_y = !(round(- mg_y / RSCS_ADXL345_OFFSET_SCALE_FACTOR) - 1);
+	if (mg_z < 0) ofs_z = !(round(- mg_z / RSCS_ADXL345_OFFSET_SCALE_FACTOR) - 1);
 
-	ofs_x = round(- mg_x / ADXL345_OFFSET_SCALE_FACTOR);
-	ofs_y = round(- mg_y / ADXL345_OFFSET_SCALE_FACTOR);
-	ofs_z = round(- mg_z / ADXL345_OFFSET_SCALE_FACTOR);
+	ofs_x = round(- mg_x / RSCS_ADXL345_OFFSET_SCALE_FACTOR);
+	ofs_y = round(- mg_y / RSCS_ADXL345_OFFSET_SCALE_FACTOR);
+	ofs_z = round(- mg_z / RSCS_ADXL345_OFFSET_SCALE_FACTOR);
 
-	rscs_adxl345_setRegisterValue(device, ADXL345_OFSX, ofs_x);
-	rscs_adxl345_setRegisterValue(device, ADXL345_OFSX, ofs_y);
-	rscs_adxl345_setRegisterValue(device, ADXL345_OFSX, ofs_z);
+	rscs_adxl345_setRegisterValue(device, RSCS_ADXL345_OFSX, ofs_x);
+	rscs_adxl345_setRegisterValue(device, RSCS_ADXL345_OFSX, ofs_y);
+	rscs_adxl345_setRegisterValue(device, RSCS_ADXL345_OFSX, ofs_z);
 
 	return error;
 }
@@ -265,7 +265,7 @@ rscs_e rscs_adxl345_read(rscs_adxl345_t * device, int16_t * x, int16_t * y, int1
 {
 	rscs_e error = RSCS_E_NONE;
 	uint8_t readBuffer[6]   = {0};
-	uint8_t firstRegAddress = ADXL345_SPI_READ | ADXL345_SPI_MB | ADXL345_DATAX0;
+	uint8_t firstRegAddress = RSCS_ADXL345_SPI_READ | RSCS_ADXL345_SPI_MB | RSCS_ADXL345_DATAX0;
 
 	switch (device->interface) {
 		case RSCS_ADXL345_SPI:
@@ -279,7 +279,7 @@ rscs_e rscs_adxl345_read(rscs_adxl345_t * device, int16_t * x, int16_t * y, int1
 		case RSCS_ADXL345_I2C:
 			GOTO_END_IF_ERROR(rscs_i2c_start());
 			GOTO_END_IF_ERROR(rscs_i2c_send_slaw(device->addr, rscs_i2c_slaw_write));
-			GOTO_END_IF_ERROR(rscs_i2c_write_byte(ADXL345_DATAX0));
+			GOTO_END_IF_ERROR(rscs_i2c_write_byte(RSCS_ADXL345_DATAX0));
 			GOTO_END_IF_ERROR(rscs_i2c_start());
 			GOTO_END_IF_ERROR(rscs_i2c_send_slaw(device->addr, rscs_i2c_slaw_read));
 			GOTO_END_IF_ERROR(rscs_i2c_read(readBuffer, sizeof(readBuffer), 1));
@@ -303,24 +303,27 @@ rscs_e rscs_adxl345_read(rscs_adxl345_t * device, int16_t * x, int16_t * y, int1
 	return error;
 }
 
+void rscs_adxl345_cast_to_G(rscs_adxl345_t * device, int16_t x, int16_t y, int16_t z, float * x_g, float * y_g, float * z_g) {
+	uint8_t  range = 1;
 
-/* ЧТЕНИЕ ДАННЫХ ADXL345 В БИНАРНОМ ВИДЕ И ПРЕОБРАЗОВАНИЕ В ЕДИНИЦЫ g */
-void ADXL345_GetGXYZ(rscs_adxl345_t * device, int16_t* x, int16_t* y, int16_t* z, float* x_g, float* y_g, float* z_g)
+	range = (1 << device->range);
+
+	if(x >> 9) *x_g = -(float)(!x + 1);	//если 10-й бит равен 1, то число отрицательное
+	if(x >> 9) *y_g = -(float)(!y + 1);
+	if(x >> 9) *z_g = -(float)(!z + 1);
+
+	*x_g = (*x_g) * RSCS_ADXL345_SCALE_FACTOR * range;
+	*y_g = (*y_g) * RSCS_ADXL345_SCALE_FACTOR * range;
+	*z_g = (*z_g) * RSCS_ADXL345_SCALE_FACTOR * range;
+}
+
+void rscs_adxl345_GetGXYZ(rscs_adxl345_t * device, int16_t* x, int16_t* y, int16_t* z, float* x_g, float* y_g, float* z_g)
 {
 	*x = 0;
 	*y = 0;
 	*z = 0;
-	uint8_t  range = 1;
-
-	for (size_t i = 0; i < (device->range); i++) range = range * 2;
 
 	rscs_adxl345_read(device, x, y, z);
 
-    if(*x >> 9) *x_g = -(float)(!*x + 1);	//если 10-й бит равен 1, то число отрицательное
-    if(*x >> 9) *y_g = -(float)(!*y + 1);
-    if(*x >> 9) *z_g = -(float)(!*z + 1);
-
-    *x_g = (*x_g) * ADXL345_SCALE_FACTOR * range;
-    *y_g = (*y_g) * ADXL345_SCALE_FACTOR * range;
-    *z_g = (*z_g) * ADXL345_SCALE_FACTOR * range;
+	rscs_adxl345_cast_to_G(device, *x, *y, *z, x_g, y_g, z_g);
 }
--- rscs/adxl345.h
+++ rscs/adxl345.h
@@ -6,43 +6,28 @@
 
 #include "error.h"
 
-/***************************************************************************/
-/******************** ИНИЦИАЛИЗАЦИЯ ПИНА CS ДЛЯ ADXL345 ********************/
-/********************      ЗАДАЁТСЯ ПОЛЬЗОВАТЕЛЕМ       ********************/
-/***************************************************************************/
-
-#define ADXL345_CS_DDR			DDRB	// регистр DDR порта, на котором расположен CS пин
-#define ADXL345_CS_PORT			PORTB	// регистр PORT порта, на котором расположен CS пин
-#define ADXL345_CS_PIN			4		// номер пина CS в порту
-
-/****************************************************************/
-/********************   НАСТРОЙКИ ADXL345    ********************/
-/******************** ЗАДАЁТСЯ ПОЛЬЗОВАТЕЛЕМ ********************/
-/****************************************************************/
-
-
 //TODO: написать функцию rscs_adxl345_low_power(bool low_power)
-#define ADXL345_BW_RATE_DATA		ADXL345_RATE(0b1010) // LOW_POWER - OFF | 1010 - 100Гц
+#define RSCS_ADXL345_BW_RATE_DATA		RSCS_ADXL345_RATE(0b1010) // LOW_POWER - OFF | 1010 - 100Гц
 
 //TODO:Выходить из спящего режима SLEEP = 0 при MEASURE = 0
-#define ADXL345_POWER_CTL_DATA		0x00 // LINK - OFF | AUTO_SLEEP - OFF | MEASURE - OFF | SLEEP - OFF | WAKEUP(x) - OFF.
+#define RSCS_ADXL345_POWER_CTL_DATA		0x00 // LINK - OFF | AUTO_SLEEP - OFF | MEASURE - OFF | SLEEP - OFF | WAKEUP(x) - OFF.
 
 //настройка событий прерывания перенести в librscs_config.h
-#define ADXL345_INT_ENABLE_DATA		ADXL345_DATA_READY // DATA_READY - ON, остальные - OFF
+#define RSCS_ADXL345_INT_ENABLE_DATA		ADXL345_DATA_READY // DATA_READY - ON, остальные - OFF
 
 //карта вывода прерываний (на INT1 или INT2) перенести в librscs_config.h
-#define ADXL345_INT_MAP_DATA		0x00 // 0 - все типы прерываний выведены на пин INT1
+#define RSCS_ADXL345_INT_MAP_DATA		0x00 // 0 - все типы прерываний выведены на пин INT1
 
 //TODO: написать функцию, запускающую self-test
 //TODO: INT_INVERT перенести в librscs_config.h
-#define ADXL345_DATA_FORMAT_DATA	ADXL345_RANGE_4G // RANGE_4G - диапазон измерений +-4g, остальные параметры по умолчанию
+#define RSCS_ADXL345_DATA_FORMAT_DATA	RSCS_ADXL345_RANGE_4G // RANGE_4G - диапазон измерений +-4g, остальные параметры по умолчанию
 
 //TODO: FIFO_CTL перенести в librscs_config.h
-#define ADXL345_FIFO_CTL_DATA		ADXL345_FIFO_STREAM   // Режим FIFO - STREAM, TRIGGER и SAMPLES не имеют значения.
+#define RSCS_ADXL345_FIFO_CTL_DATA		RSCS_ADXL345_FIFO_STREAM   // Режим FIFO - STREAM, TRIGGER и SAMPLES не имеют значения.
 
 /* ADXL345_BW_RATE Определение регистра */
-#define ADXL345_LOW_POWER       (1 << 4)
-#define ADXL345_RATE(x)         ((x) & 0xF)
+#define RSCS_ADXL345_LOW_POWER       (1 << 4)
+#define RSCS_ADXL345_RATE(x)         ((x) & 0xF)
 
 
 
@@ -50,18 +35,18 @@
 
 
 /* ADXL345_FIFO_CTL Определение регистра */
-#define ADXL345_FIFO_MODE(x)    (((x) & 0x3) << 6)
-#define ADXL345_TRIGGER         (1 << 5)
-#define ADXL345_SAMPLES(x)      ((x) & 0x1F)
+#define RSCS_ADXL345_FIFO_MODE(x)    (((x) & 0x3) << 6)
+#define RSCS_ADXL345_TRIGGER         (1 << 5)
+#define RSCS_ADXL345_SAMPLES(x)      ((x) & 0x1F)
 
 /* ADXL345_FIFO_MODE(x) Возможные значения */
-#define ADXL345_FIFO_BYPASS     0
-#define ADXL345_FIFO_FIFO       1
-#define ADXL345_FIFO_STREAM     2
-#define ADXL345_FIFO_TRIGGER    3
+#define RSCS_ADXL345_FIFO_BYPASS     0
+#define RSCS_ADXL345_FIFO_FIFO       1
+#define RSCS_ADXL345_FIFO_STREAM     2
+#define RSCS_ADXL345_FIFO_TRIGGER    3
 
 /* ADXL345_FIFO_STATUS Определение регистра */
-#define ADXL345_FIFO_TRIG       (1 << 7)
+#define RSCS_ADXL345_FIFO_TRIG       (1 << 7)
 #define ADXL345_ENTRIES(x)      ((x) & 0x3F)
 
 
@@ -125,7 +110,7 @@ typedef enum
 	RSCS_ADXL345_RATE_3200HZ	= 0b1111,	//  3200 Герц
 } rscs_adxl345_rate_t;
 
-rscs_e rscs_adxl345_getRegisterValue(rscs_adxl345_t * device, uint8_t registerAddress, uint16_t/*!!!*/ * read_data);
+rscs_e rscs_adxl345_getRegisterValue(rscs_adxl345_t * device, uint8_t registerAddress, uint8_t * read_data);
 
 
 
@@ -160,8 +145,11 @@ rscs_e rscs_adxl345_set_rate(rscs_adxl345_t * device, rscs_adxl345_rate_t rate);
 */
 rscs_e rscs_adxl345_read(rscs_adxl345_t * device, int16_t * x, int16_t * y, int16_t * z);
 
+// Перевод сырых данных акселерометра в G
+void rscs_adxl345_cast_to_G(rscs_adxl345_t * device, int16_t x, int16_t y, int16_t z, float * x_g, float * y_g, float * z_g);
 
-void ADXL345_GetGXYZ(rscs_adxl345_t * device, int16_t* x, int16_t* y, int16_t* z, float* x_g, float* y_g, float* z_g);
+/* ЧТЕНИЕ ДАННЫХ ADXL345 В БИНАРНОМ ВИДЕ И ПРЕОБРАЗОВАНИЕ В ЕДИНИЦЫ g */
+void rscs_adxl345_GetGXYZ(rscs_adxl345_t * device, int16_t* x, int16_t* y, int16_t* z, float* x_g, float* y_g, float* z_g);
 
 
 #endif /* ADXL345_H_ */
