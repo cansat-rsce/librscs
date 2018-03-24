01433bb43bf4554029eea915ea26201940468efb
developer <developer@cansat-workstation>
2018-03-24 21:16:35 +0300

    CDM7160 started

--- rscs/_source/cdm7160.c
+++ rscs/_source/cdm7160.c
@@ -4,25 +4,102 @@
  *  Created on: 24 марта 2018 г.
  *      Author: developer
  */
-
+#include <stdlib.h>
 #include "error.h"
-
+#include "stddef.h"
 #include "cdm7160.h"
 
-#define addr_H 0b1101001
-#define addr_L 0b1101000
 
-rscs_e rscs_cdm7160_reset(void)
-{
-	rscs_e error = rscs_i2c_start();
-	if(error != 0) return error;
-	error = rscs_i2c_send_slaw(addr_H, rscs_i2c_slaw_write);
-	if(error != 0) return error;
-	error = rscs_i2c_write_byte(0x00);
-	if(error != 0) return error;
-	error = rscs_i2c_write_byte(0x01);
-	if(error != 0) return error;
-	rscs_i2c_stop();
+rscs_cdm7160_sensor_t * rscs_cdm7160_init(addr){
+	rscs_sdcard_t * self = (rscs_sdcard_t*)malloc(sizeof(rscs_sdcard_t));
+	rscs_cdm7160_sensor_t* sensor = (rscs_cdm7160_sensor_t*)malloc(sizeof(rscs_cdm7160_sensor_t));
+	if(NULL == sensor) return sensor;
+
+	if(addr == LOW) sensor->addr = 0b1101000;
+	if(addr == HIGH) sensor->addr = 0b1101001;
+
+	return sensor;
 }
+
+
+rscs_e rscs_cdm7160_reset(rscs_cdm7160_sensor_t* sensor)
+{
+	error = rscs_i2c_start();
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_send_slaw(sensor->addr, rscs_i2c_slaw_write);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(0x00);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(0x01);
+	if(error != RSCS_E_NONE) goto end;
+
+	end:
+				rscs_i2c_stop();
+				return error;
+};
+
+rscs_e rscs_cdm7160_mode_set(rscs_cdm7160_sensor_t* sensor, rscs_cdm7160_mode_t* mode)
+{
+	error = rscs_i2c_start();
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_send_slaw(sensor->addr, rscs_i2c_slaw_write);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(0x01);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(mode);
+	if(error != RSCS_E_NONE) goto end;
+
+	end:
+			rscs_i2c_stop();
+			return error;
+};
+
+rscs_e rscs_cdm7160_read_CO2(rscs_cdm7160_sensor_t* sensor)
+{
+	error = rscs_i2c_start();
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_send_slaw(sensor->addr, rscs_i2c_slaw_read);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(0x03);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_read(void * data_ptr, 2, true);
+	if(error != RSCS_E_NONE) goto end;
+
+	end:
+				rscs_i2c_stop();
+				return error;
+};
+
+rscs_e rscs_cdm7160_write_pressure_corr(rscs_cdm7160_sensor_t* sensor, int pressure)
+{
+	error = rscs_i2c_start();
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_send_slaw(sensor->addr, rscs_i2c_slaw_write);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(0x09);
+	if(error != RSCS_E_NONE) goto end;
+	error = rscs_i2c_write_byte(pressure);
+	if(error != RSCS_E_NONE) goto end;
+
+		end:
+				rscs_i2c_stop();
+				return error;
+};
+
+rscs_e rscs_cdm7160_write_altitude_corr(rscs_cdm7160_sensor_t* sensor, int altitude)
+{
+	error = rscs_i2c_start();
+		if(error != RSCS_E_NONE) goto end;
+		error = rscs_i2c_send_slaw(sensor->addr, rscs_i2c_slaw_write);
+		if(error != RSCS_E_NONE) goto end;
+		error = rscs_i2c_write_byte(0x0A);
+		if(error != RSCS_E_NONE) goto end;
+		error = rscs_i2c_write_byte(altitude);
+		if(error != RSCS_E_NONE) goto end;
+
+		end:
+				rscs_i2c_stop();
+				return error;
+};
 
 
--- rscs/cdm7160.h
+++ rscs/cdm7160.h
@@ -4,6 +4,7 @@
  *  Created on: 24 марта 2018 г.
  *      Author: developer
  */
+/* Библиотека для работы с инфракрасным датчиком СО2 cdm7160 по шине I2C */
 
 #ifndef CDM7160_H_
 #define CDM7160_H_
@@ -15,3 +16,28 @@
 
 rscs_e rscs_cdm7160_reset(void); // функция осуществляет программный reset устройства
 rscs_e rscs_cdm7169_init(void); // инициализирует cdm7160
+
+typedef enum{
+	CONTINUOUS = 0x06,
+	SLEEP = 0x00,
+} rscs_cdm7160_mode_t; // Перечисление режимов работы модуля
+
+typedef enum{
+	HIGH,
+	LOW,
+} rscs_cdm7160_address_t; // Перечисление возможных адресов
+
+
+typedef struct {
+	uint8_t addr;
+} rscs_cdm7160_sensor_t; // Объявление типа структуры для хранения дескриптора устройства
+
+rscs_e rscs_cdm7160_init(rscs_cdm7160_address_t); // Функция создаёт дескриптор устройства на шине
+rscs_e rscs_cdm7160_reset(rscs_cdm7160_sensor_t); // Функция осуществляет программный reset устройства
+rscs_e rscs_cdm7160_mode_set(rscs_cdm7160_sensor_t, rscs_cdm7160_mode_t); // функция выбора режима работы датчика (постоянное измерение/спящий режим)
+rscs_e rscs_cdm7160_readCO2(rscs_cdm7160_sensor_t); // Функция чтения показаний концентрации CO2
+rscs_e rscs_cdm7160_write_pressure_corr(rscs_cdm7160_sensor_t, int pressure); // Функция коррекции показаний с учётом атмосферного давления
+rscs_e rscs_cdm7160_write_altitude_corr(rscs_cdm7160_sensor_t, int altitude); // Функция коррекции показаний с учётом высоты
+
+
+#endif /* CDM7160_H_ */
